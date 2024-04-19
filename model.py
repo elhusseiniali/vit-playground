5a82d17de4a8f4d12a808c92f84d7f44a3b6c27f
Ali El Husseini <elhusseiniali@icloud.com>
2024-04-19 18:11:51 +0800

    Add relu attribute to constructor.

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class ViTForClassfication(nn.Module):
     The ViT model for classification.
     """
 
-    def __init__(self, config, performer=False, m=16):
+    def __init__(self, config, relu=False, m=16):
         super().__init__()
         self.config = config
         self.image_size = config["image_size"]
@@ -19,9 +19,9 @@ class ViTForClassfication(nn.Module):
         # Create the embedding module
         self.embedding = Embeddings(config)
         # Create the transformer encoder module
-        self.performer = performer
+        self.relu = relu
         self.m = m
-        self.encoder = Encoder(config, performer=self.performer, m=self.m)
+        self.encoder = Encoder(config, relu=self.relu, m=self.m)
         # Create a linear layer to project the encoder's output to 
         # the number of classes
         self.classifier = nn.Linear(self.hidden_size, self.num_classes)
